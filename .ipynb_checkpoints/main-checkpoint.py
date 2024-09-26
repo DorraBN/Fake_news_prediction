0d8cb1ce5e1228a663ae5654897714905e547b8d
DorraBN <dorransira18@gmail.com>
2024-09-26 10:39:42 +0100

    first commit

---
+++ .ipynb_checkpoints/main-checkpoint.py
@@ -0,0 +1,6 @@
+import random
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.models import Sequential  # Correct spelling for 'Sequential'
+from tensorflow.keras.layers import LSTM, Dense, Activation  # Correct spelling for 'Activation'
+from tensorflow.keras.optimizers import RMSprop
